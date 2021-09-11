7700087bb5a8e93f9b9c41400e2d2dac7e07e198
Maximo Junior Apaza Chirhuana <maximopeoficiales@gmail.com>
2021-09-11 05:13:21 -0500

    ADD: correcion en route client

--- src/web/routes/clients.route.ts
+++ src/web/routes/clients.route.ts
@@ -3,7 +3,7 @@ import { clientController } from "../controllers/client.controller";
 import { validation } from "../middlewares/validation.middleware";
 import { clientSchema } from "../middlewares/validations/client.schema";
 
-router.get("/clients", clientController.index).get;
+router.get("/clients", clientController.index);
 router.post("/clients", validation(clientSchema), clientController.save);
 router.get("/clients/dashboard", clientController.getDataDashboard);
 
